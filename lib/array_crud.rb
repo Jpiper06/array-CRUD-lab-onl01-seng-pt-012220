3a45a3a1e21175dff266bd55086318d45709d80b
Jaquan Piper <jaquan.piper@gmail.com>
2020-01-30 20:00:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,24 @@
 def create_an_empty_array
+[]
   
 end
 
 def create_an_array
-  
+ 
+  int mark [5] = {19, 10, 8, 17, 9}
+
 end
 
 def add_element_to_end_of_array(array, element)
+  
+  push.(12)
   
 end
 
 def add_element_to_start_of_array(array, element)
   
+  unshift.(22)
+
 end
 
 def remove_element_from_end_of_array(array)
